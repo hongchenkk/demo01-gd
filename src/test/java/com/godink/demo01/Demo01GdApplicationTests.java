79838a0a179559446e9feffbaba5bd62a9204904
hong.chen <hong.chen@audaque.com>
2020-10-26 18:27:26 +0800

    commit

---
+++ src/test/java/com/godink/demo01/Demo01GdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godink.demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01GdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
